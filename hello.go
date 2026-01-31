76e61b3b631cd4d9290034900e41a5a5c30cae90
xytgy <xytgy99@gmail.com>
2026-01-31 21:44:43 +0800

    feat: 添加Go语言示例程序hello.go
    添加一个简单的Go语言程序，演示变量声明、初始化和基本输出功能。程序包含多个示例，展示不同数据类型的默认值（零值）和格式化输出方法。

---
+++ hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// %d 表示整型数字，%s 表示字符串
+	// var stockcode = 123
+	// var enddate = "2020-12-31"
+	// var url = "Code=%d&endDate=%s"
+	// var target_url = fmt.Sprintf(url, stockcode, enddate)
+	// fmt.Println(target_url)
+
+	// var a = "Runoon"
+	// fmt.Println(a)
+
+	// var b, c = 123, "2020-12-31"
+	// fmt.Println(b, c)
+	var b int
+	fmt.Println(b)
+
+	// bool 零值为 false
+	var c bool
+	fmt.Println(c)
+}
